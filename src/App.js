45991b3d76fa0d2585ad8d2b5ae58791534ec666
sultan bekov <bekovs96@gmail.com>
2023-06-22 12:15:21 +0600

    point2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AppRoutes from './routes/AppRoutes';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <AppRoutes />
+    </>
   );
 };
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Navbar.css"
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+
+  const navigate = useNavigate()
+
+  return (
+    <div className='navbar'>
+      <ul className='navbar__list'>
+        <li onClick={() => navigate('/')}>Home</li>
+        <li onClick={() => navigate('/about')}>About us</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
